96b36e5ddb44812be92ce57c5cb21b6a4b8abe64
manunio <max.manu.nair@gmail.com>
2021-09-18 04:12:33 +0530

    Updated GetItemAsync_WithUnExistingItem_ReturnsNotFound to use FluentAssertions.

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -34,7 +34,7 @@ namespace Catalog.UnitTests
             var result = await controller.GetItemAsync(Guid.NewGuid());
 
             // Assert
-            Assert.IsType<NotFoundResult>(result.Result);
+            result.Result.Should().BeOfType<NotFoundResult>();
         }
 
         [Fact]
