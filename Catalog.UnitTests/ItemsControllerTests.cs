0e9ea6e8ecf28898213e01e5290816fe8b7f747f
manunio <max.manu.nair@gmail.com>
2021-09-18 04:01:19 +0530

    Added a new unit test case fot GetItemsAsync

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Catalog.Api.Controllers;
+using Catalog.Api.Dtos;
 using Catalog.Api.Entities;
 using Catalog.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -12,21 +13,23 @@ namespace Catalog.UnitTests
 {
     public class ItemsControllerTests
     {
+        private readonly Mock<IItemsRepository> _repositoryStub = new();
+        private readonly Mock<ILogger<ItemsController>> _loggerStub = new();
+        private readonly Random _rand = new();
+
         [Fact]
         // UnitOfWork_StateUnderTests_ExpectedBehaviour()
         public async Task GetItemAsync_WithUnExistingItem_ReturnsNotFound()
         {
             // Arrange
-            var repositoryStub = new Mock<IItemsRepository>();
+
             // whenever controller invokes GetItemAsync with any Guid,
             // which moq is going to provide. It have to return a null value.
-            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+            _repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
                 .ReturnsAsync((Item) null);
 
-            var loggerStub = new Mock<ILogger<ItemsController>>();
-            var controller =
-                new ItemsController(repositoryStub.Object,
-                    loggerStub.Object); //.Object passes the object of type instead of moq
+            //.Object passes the object of type instead of moq
+            var controller = new ItemsController(_repositoryStub.Object, _loggerStub.Object);
 
             // Act
             var result = await controller.GetItemAsync(Guid.NewGuid());
@@ -38,6 +41,34 @@ namespace Catalog.UnitTests
         [Fact]
         public async Task GetItemAsync_WithUnExistingItem_ReturnsExpectedItem()
         {
+            //Arrange
+            var expectedItem = CreateRandomItem();
+            _repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(expectedItem);
+            var controller = new ItemsController(_repositoryStub.Object, _loggerStub.Object);
+
+            // Act
+            var result = await controller.GetItemAsync(Guid.NewGuid());
+
+            //Assert
+            Assert.IsType<ItemDto>(result.Value); //Value property has the dto.
+            var dto = ((ActionResult<ItemDto>) result).Value;
+            Assert.Equal(expectedItem.Id, dto.Id);
+            Assert.Equal(expectedItem.Name, dto.Name);
+            Assert.Equal(expectedItem.Name, dto.Name);
+            
+        }
+
+        private Item CreateRandomItem()
+        {
+            return new()
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = _rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow,
+                UpdatedDate = DateTimeOffset.UtcNow
+            };
         }
     }
 }
